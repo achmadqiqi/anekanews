e3d21fbecb5a3423c521bcb4e9bc884706f22e98
Ragapool <achmadqiqi888@gmail.com>
2026-08-05 18:18:35 +0700

    fix: update Kuliner article with dedicated relevant food photo

---
+++ scratch/update_kuliner_article_image.sql
@@ -0,0 +1,3 @@
+UPDATE published_articles
+SET image_url = '/images/mengenal-kuliner-khas-kediri-tahu-takwa.webp'
+WHERE slug = 'menjelajah-kekayaan-kuliner-khas-kediri-tahu-takwa-nasi-goreng-arang';
